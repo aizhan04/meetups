fd7ac78dd478876638164e3e099bc5ec2157dca3
aizhan04 <aizhan.makhmutzhanova@gmail.com>
2022-07-01 19:31:02 +0600

    login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,30 +3,92 @@
 		<div class="form-wrapper">
 			<h2>Вход</h2>
 
-			<form class="login-form">
+			<form class="login-form" @submit.prevent="submitForm">
 				<div class="form-group">
 					<label for="email">Email</label>
-					<input type="email" id="email" />
+					<input type="email" id="email" placeholder="email" v-model="email" />
 				</div>
 
 				<div class="form-group">
 					<label for="password">Пароль</label>
-					<input type="password" id="password" />
+					<input
+						type="password"
+						id="password"
+						placeholder="password"
+						v-model="password"
+					/>
 				</div>
+				<p :class="{ active: isEmpty }" class="error-text">
+					Заполните все поля
+				</p>
+				<p :class="{ active: isNotActive }" class="error-text">
+					Неверный логин или пароль
+				</p>
 
-				<button>Войти</button>
+				<button v-if="loading"><v-loader /></button>
+				<button v-else>Вход</button>
 			</form>
 
-			<!-- <p class="form-question">
-				Нет аккаунта?<router-link to="/register">Зарегестрируйтесь</router-link>
-			</p> -->
+			<p class="form-question">
+				Нет аккаунта?<router-link to="/register" class="question-text">
+					Зарегестрируйтесь</router-link
+				>
+			</p>
 		</div>
 	</div>
 </template>
 
 <script>
+import VLoader from "@/components/VLoader.vue";
 export default {
+	components: { VLoader },
+
 	name: "Login",
+	data() {
+		return {
+			email: "",
+			password: "",
+			isEmpty: false,
+			isNotActive: false,
+			loading: false,
+		};
+	},
+
+	methods: {
+		async submitForm() {
+			if (!this.email || !this.password) {
+				this.isEmpty = true;
+				return;
+			}
+			const info = {
+				email: this.email,
+				password: this.password,
+			};
+			console.log(info);
+			this.loading = true;
+			try {
+				const response = await fetch(
+					"https://course-vue.javascript.ru/api/auth/login/",
+					{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json;charset=utf-8",
+						},
+						body: JSON.stringify(info),
+					}
+				);
+				if (response.status === 403)
+					throw new Error("403 is unacceptable for me!");
+
+				const data = await response.json();
+				console.log(data);
+			} catch (error) {
+				this.isNotActive = true;
+			}
+
+			this.loading = false;
+		},
+	},
 };
 </script>
 
@@ -57,6 +119,14 @@ h2 {
 	margin-bottom: 20px;
 }
 
+.error-text {
+	color: #db3851;
+	display: none;
+}
+
+.active {
+	display: unset;
+}
 .form-group label {
 	font-size: 20px;
 	line-height: 28px;
@@ -108,11 +178,16 @@ h2 {
 .form-wrapper button:hover {
 	background-color: #b8c5e2;
 }
-/* 
+
 .form-question {
 	margin-top: 40px;
 	text-align: center;
 	font-size: 20px;
 	line-height: 28px;
-} */
+}
+
+.question-text {
+	text-decoration: none;
+	color: #98aace;
+}
 </style>
